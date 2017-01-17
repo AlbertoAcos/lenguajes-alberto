089887a7e54ed0b141eeeda2ab255c87f10bade3
AlbertoAcos <jaac2094@gmail.com>
2017-01-16 19:36:58 -0600

    Update Lenjuajes.java

--- lenjuajes/src/lenjuajes/Lenjuajes.java
+++ lenjuajes/src/lenjuajes/Lenjuajes.java
@@ -15,7 +15,7 @@ public class Lenjuajes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      System.out.println("Hola mundo");
+      System.out.println("Hola mundo pero desee github");
     }
     
 }
